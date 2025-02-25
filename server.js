a9664170ef0fe81a3f7df3a91544abc48d81eda4
Intira Wichitanurak <wichitanuraksxinthira@gmail.com>
2025-02-25 15:59:49 +0700

    commit

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // เชื่อมต่อ MySQL
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123456789",
-  database: "mini",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
